89c7a147df669f13a9cd65065c535af9c1dd35b6
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-24 17:43:51 -0500

    cree la base para crear cursos

--- main.js
+++ main.js
@@ -1,44 +1,39 @@
-const form = document.getElementById('formulario');
-const nombreInput = document.getElementById('nombre');
-const saludo = document.getElementById('saludo');
-const btnBorrar = document.getElementById('borrar');
-
-
-
-
+const form = document.getElementById('formulario-curso');
+const inputCurso = document.getElementById('nombre-curso');
+const inputProfesor = document.getElementById('nombre-profesor');
+const inputPrecio = document.getElementById('precio');
+const inputCiudad = document.getElementById('ciudad');
+const inputCupo = document.getElementById('cupo')
+const msjCurso = document.getElementById('mensaje-curso');
+const btnBorrar = document.getElementById('borrar-curso');
 
 
 form.addEventListener('submit' , (e)=>{
+
     e.preventDefault();
-    const userName = nombreInput.value.trim();
+
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
 
 
-    if (userName=="") {
-        alert("marque un nombre manito")
+    if (curso=="" || profesor=="" || precio=="" || ciudad=="" || cupo=="") {
+        alert('termine de llenar todo ole')
         return;
     }
-    localStorage.setItem('usuario' , userName);
+// construimos un objeto con caracteristicas con sus diferentes claves y valores
+const nuevoCurso = {
+    curso: curso,
+    profesor: profesor,
+    precio: precio,
+    ciudad: ciudad,
+    cupo: cupo
+}
 
-    saludo.textContent='Bienvenido ' + userName
-
-    nombreInput.value="";
-});
-
-
-document.addEventListener('DOMContentLoaded' , ()=>{
-
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if (usuarioGuardado) {
-       saludo.textContent = 'Bienvenido de nuevo ' + usuarioGuardado
-    }
-});
-
-
-btnBorrar.addEventListener('click' , ()=>{
-
-    localStorage.removeItem('usuario');
-    saludo.textContent = 'Bienvenido usuario indefinido'
+localStorage.setItem('curso' , JSON.stringify(nuevoCurso))
+form.reset();
 
 });
-
 
