40a094d3afa63656ea15896396f95eeda7ceec7a
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-26 16:40:48 -0500

    agregue una otro archivo html para que muestre las tablas que se van creando

--- cursos-table.js
+++ cursos-table.js
@@ -2,5 +2,38 @@ const table = document.querySelector('#tablaCursos tbody');
 
 
 document.addEventListener('DOMContentLoaded' , ()=>{
-    
+    const cursosGuardados = JSON.parse(localStorage.getItem('cursos')) || [];
+
+    cursosGuardados.forEach((curso) => {
+        const fila = document.createElement('tr');
+
+        const tdNombre = document.createElement('td');
+            tdNombre.textContent = curso.curso;
+
+        const tdProfesor = document.createElement('td');
+            tdProfesor.textContent = curso.profesor;
+
+        const tdPrecio = document.createElement('td');
+            tdPrecio.textContent = curso.precio;
+
+        const tdCiudad = document.createElement('td');
+            tdCiudad.textContent = curso.ciudad;
+
+        const tdCupo = document.createElement('td');
+            tdCupo.textContent = curso.cupo;
+
+
+        // ensamblamos todos los elementos creado anteriormente
+
+        fila.appendChild(tdNombre);
+        fila.appendChild(tdProfesor);
+        fila.appendChild(tdPrecio);
+        fila.appendChild(tdCiudad);
+        fila.appendChild(tdCupo);
+        table.appendChild(fila);
+
+
+        // utilizamos el return para retornar o dar respuesta del elemento creado ya que lo usaremos en otra funcion
+    });
+
 })
